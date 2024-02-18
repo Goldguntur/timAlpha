a0ab7ddc95bc867bf4da75deb433bd34dcf8b173
gunturPutra <goldguntur1623@gmail.com>
2024-02-18 14:37:49 +0700

    menyelesaikan section instansi design awal

--- assest/script/instansi.js
+++ assest/script/instansi.js
@@ -1,13 +1,20 @@
-const instansi = document.querySelector(".instansi")
+const instansi = document.querySelector(".instansi");
 
 const yrbnBtn = document.querySelectorAll(".button-second")[0];
 const smpBtn = document.querySelectorAll(".button-second")[1];
-const smkBtn = document.querySelectorAll(".button-second")[2]
+const smkBtn = document.querySelectorAll(".button-second")[2];
 
 const yrbn = `<div>
   <h1 class="rh1 mb-10 text-center">Balanta</h1> 
   <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia delectus facere ullam iusto deleniti, maxime vero ipsum praesentium expedita. Quaerat suscipit velit dicta facilis, earum voluptatum ea maxime voluptatibus maiores, voluptate temporibus at in eius quos quod dolorem. Eius, maxime!</p>
-</div>
+  <ul class="list-image-[url(../assest/images/list/list.png)] list-inside mx-auto text-nowrap mt-8">
+      <li>desain visual komunikasi</li>
+      <li>perhotelan</li>
+      <li>manajemen perkantoran lembaga bisnis</li>
+      <li>akutasni</li>
+</ul>
+
+  </div>
 <div class="text-center">
 <img src="../assest/images/tbb.png" alt="icon" class="lg:w-[1200px] ">
 <div class="relative flex flex-col text-gray-700 bg-gray-200 shadow-lg bg-clip-border rounded-xl w-52 mx-auto mt-10">
@@ -28,9 +35,16 @@ const yrbn = `<div>
 </div>
 </div>`;
 
-const smp =`<div>
+const smp = `<div>
 <h1 class="rh1 mb-10 text-center">Smp prima bangsa</h1> 
 <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia delectus facere ullam iusto deleniti, maxime vero ipsum praesentium expedita. Quaerat suscipit velit dicta facilis, earum voluptatum ea maxime voluptatibus maiores, voluptate temporibus at in eius quos quod dolorem. Eius, maxime!</p>
+<ul class="list-image-[url(../assest/images/list/list.png)] list-inside mx-auto text-nowrap mt-8">
+   <li>desain visual komunikasi</li>
+   <li>perhotelan</li>
+   <li>manajemen perkantoran lembaga bisnis</li>
+   <li>akutasni</li>
+</ul>
+
 </div>
 <div class="text-center">
 <img src="../assest/images/Pb.png" alt="icon" class="lg:w-[1200px] ">
@@ -52,9 +66,15 @@ const smp =`<div>
 </div>
 </div>`;
 
-const smk =`<div>
+const smk = `<div>
 <h1 class="rh1 mb-10 text-center">Smk Taruna Budi Bangsa</h1> 
 <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia delectus facere ullam iusto deleniti, maxime vero ipsum praesentium expedita. Quaerat suscipit velit dicta facilis, earum voluptatum ea maxime voluptatibus maiores, voluptate temporibus at in eius quos quod dolorem. Eius, maxime!</p>
+<ul class="list-image-[url(../assest/images/list/list.png)] list-inside mx-auto text-nowrap mt-8">
+  <li>desain visual komunikasi</li>
+  <li>perhotelan</li>
+  <li>manajemen perkantoran lembaga bisnis</li>
+  <li>akutasni</li>
+</ul>
 </div>
 <div class="text-center">
 <img src="../assest/images/tbb.png" alt="icon" class="lg:w-[1200px] ">
@@ -76,15 +96,14 @@ const smk =`<div>
 </div>
 </div>`;
 
-
 yrbnBtn.addEventListener("click", () => {
-    instansi.innerHTML = yrbn;
-})
+  instansi.innerHTML = yrbn;
+});
 
 smpBtn.addEventListener("click", () => {
-    instansi.innerHTML = smp;
-})
+  instansi.innerHTML = smp;
+});
 
 smkBtn.addEventListener("click", () => {
-    instansi.innerHTML = smk;
-})
+  instansi.innerHTML = smk;
+});
