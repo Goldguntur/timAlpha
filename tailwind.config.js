a8e8c49e3b9606af0b0171c4661e04a66944d3e4
gunturPutra <goldguntur1623@gmail.com>
2024-03-13 14:17:09 +0700

    memulai penulisan gallery js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
-
 module.exports = {
   content: ["./yayasan/**/*.{html,js}"],
   theme: {
     fontFamily: {
       arial: ["arial"],
+      serif: "sans-serif",
     },
     extend: {},
   },
