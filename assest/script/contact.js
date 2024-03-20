689bacee868aef46def5afbcd7ff7786a3309f47
gunturPutra <goldguntur1623@gmail.com>
2024-03-20 17:18:25 +0700

    menambahkan script dan mengupdate script contact client side

--- assest/script/contact.js
+++ assest/script/contact.js
@@ -1,124 +1,30 @@
-const form = document.getElementById('contact-form')
-const inputName = document.getElementById('nama')
-const inputEmail = document.getElementById('email')
-const inputSubject = document.getElementById('subject')
-const inputDesk = document.getElementById('deskripsi')
+const form = document.querySelectorAll('section#contact div.container form input');
+const btnSend = document.querySelector('section#contact div.container form button')
+const small = document.querySelectorAll('section#contact div.container form small')
+const contact = document.getElementById('contact-form')
+
+const from = Array.from(form)
 
 
-const isRequired = value => value === '' ? false : true;
 
-const checkName = () => {
+contact.addEventListener('submit', (e) => {
+     e.preventDefault()
 
-    let valid = false;
-
-    const names = inputName.value.trim();
-
-    if (!isRequired(names)) {
-        showError(inputName, 'nama tidak boleh kosong');
-    } else {
-        showSuccess(inputName);
-        valid = true;
-    }
-    return valid;
-};
-
-const checkEmail = () => {
-    let valid = false;
-    const email = inputEmail.value.trim();
-    if (!isRequired(email)) {
-        showError(inputEmail, 'Email tidak boleh kosong.');
-    } else {
-        showSuccess(inputEmail);
-        valid = true;
-    }
-    return valid;
-};
-const checkSubject = () => {
-    let valid = false;
-    const subject = inputSubject.value.trim();
-    if (!isRequired(subject)) {
-        showError(inputSubject, 'Subjek tidak boleh kosong.');
-    } else {
-        showSuccess(inputSubject);
-        valid = true;
-    }
-    return valid;
-};
-
-const checkDesk = () => {
-    let valid = false;
-    const desk = inputDesk.value.trim();
-    if (!isRequired(desk)) {
-        showError(inputDesk, 'Subjek tidak boleh kosong.');
-    } else {
-        showSuccess(inputDesk);
-        valid = true;
-    }
-    return valid;
-};
-
-
-const showError = (input, message) => {
-    const formField = input.parentElement;
-   
-    formField.classList.remove('success');
-    formField.classList.add('error');
-
- 
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-
-const showSuccess = (input) => {
-
-    const formField = input.parentElement;
-
- 
-    formField.classList.remove('error');
-    formField.classList.add('success');
-
-   
-    const error = formField.querySelector('small');
-    error.textContent = '';
-}
-
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault()
-
-    checkName();
-    checkEmail();
-    checkSubject();
-    checkDesk();
-
+    from.forEach((f, i) => {
+        if (!f.value.trim()) {
+            form[i].setAttribute('required', 'required')
+          
+        } else {
+            form.removeAttribute('required')
+        }
+    })
     
 })
 
-const debounce = (fn, delay = 500) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
+from.forEach((f, i) => {
+    f.addEventListener('keyup', () => {
+        if (from[i].value.length >= 0) {
+            from[i].removeAttribute('required')
         }
-        timeoutId = setTimeout(() => {
-            fn.apply(null, args)
-        }, delay);
-    };
-};
-
-form.addEventListener('input', debounce(function (e) {
-    switch (e.target.id) {
-        case 'nama':
-            checkName();
-            break;
-        case 'email':
-            checkEmail();
-            break;
-        case 'subject':
-            checkSubject();
-            break;
-        case 'deskripsi':
-            checkDesk();
-            break;
-    }
-}));
+    })
+})
